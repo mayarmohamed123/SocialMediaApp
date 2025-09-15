8472e078267c99d145d6c07ea6ea257835420a61
Mayar <mayar201500@gmail.com>
2025-09-15 07:38:26 +0300

    fix small error

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import ProtectedRoutes from "../ProtectedRoutes/ProtectedRoutes";
 
 export default function MainLayout() {
   return (
